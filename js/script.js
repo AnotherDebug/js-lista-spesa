faa7876bbf118deae2c49bb636bae526cef8e7e4
AnotherDebug <antonio.faddanno@gmail.com>
2023-09-14 14:29:42 +0200

    [UPDATE]: file script

---
+++ js/script.js
@@ -0,0 +1,13 @@
+/* 
+Data una lista della spesa, stampare sulla pagina (anche brutalmente, basta che si vedano) gli elementi della lista individualmente con un ciclo while.
+
+1. Creo una variabile lista della spesa [];
+
+2. Creo una variabile contatore per il ciclo while;
+
+3. Utilizzo il ciclo while con una determinata condizione affichè parta il ciclo;
+
+4. All'interno del ciclo incremento la variabile contatore;
+
+5. Utilizzando la condizione del ciclo con contatore come indice, stampo uno per uno il valore della lista della spesa.
+*/
